28eebb48dcbe5ab0ebd3d54c25b800754909fcf1
Maggie <maggievu91@gmail.com>
2018-10-11 04:08:28 -0700

    Updates

---
+++ precache-manifest.aa140c69f6fc5428676d09038bf948b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeab848c26357f3d120f3e58f1efa8f5",
+    "url": "/react-landing-page/static/media/search-solid.aeab848c.svg"
+  },
+  {
+    "revision": "e0b0bd1b4cf405acb7c28c1017b79a15",
+    "url": "/react-landing-page/static/media/deforestation.e0b0bd1b.jpg"
+  },
+  {
+    "revision": "46ba8d5a240b7d0333fe384221a7ed5e",
+    "url": "/react-landing-page/static/media/cooling-tower.46ba8d5a.jpg"
+  },
+  {
+    "revision": "20a99c0383beb00c78cdd8f9af625180",
+    "url": "/react-landing-page/static/media/climate-change.20a99c03.jpg"
+  },
+  {
+    "revision": "959c3028702d48359fbe94780ad92c81",
+    "url": "/react-landing-page/static/media/air-pollution.959c3028.jpg"
+  },
+  {
+    "revision": "fef35db4759a96441639",
+    "url": "/react-landing-page/static/js/runtime~main.fef35db4.js"
+  },
+  {
+    "revision": "faeaa927c3b800bc515e",
+    "url": "/react-landing-page/static/js/main.faeaa927.chunk.js"
+  },
+  {
+    "revision": "5295d1a518a8596fa5e5",
+    "url": "/react-landing-page/static/js/1.5295d1a5.chunk.js"
+  },
+  {
+    "revision": "faeaa927c3b800bc515e",
+    "url": "/react-landing-page/static/css/main.b4ca293b.chunk.css"
+  },
+  {
+    "revision": "9d1a5fe8d797acc5ef1fb339bbb33479",
+    "url": "/react-landing-page/index.html"
+  }
+];
